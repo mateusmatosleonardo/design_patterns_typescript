71c0a7954d1f4ec06ddbe54f865266776932c4eb
Mateus Leonardo <m4teusmatos@gmail.com>
2022-12-07 21:40:03 -0300

    update class Payment

--- SOLID/5-DIP/payment/Payment.ts
+++ SOLID/5-DIP/payment/Payment.ts
@@ -1,8 +1,8 @@
-import { MySQLProduct } from "../model/MySQLProduct";
+import { DbProductFactory } from "../factory/DbProductFactory";
 
 export class Payment {
   pay(productId: number): void {
-    const dbProduct = new MySQLProduct();
+    const dbProduct = DbProductFactory.create();
     const product = dbProduct.getProductById(productId);
 
     console.log(product);
