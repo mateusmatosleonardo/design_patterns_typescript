9ce1938289c4311d40bc67cc8134f5aa2d467f01
Mateus Leonardo <m4teusmatos@gmail.com>
2022-12-13 14:37:36 -0300

    create index

---
+++ FACTORY-METHOD/index.ts
@@ -0,0 +1,17 @@
+import CarTransport from "./transport/CarTransport";
+import MotorcycleTransport from "./transport/MotorcycleTransport";
+import { Transport } from "./transport/Transport";
+
+declare var process: any;
+
+let transport: Transport;
+
+if (process.argv.includes("--uber")) {
+  transport = new CarTransport();
+} else if (process.argv.includes("--log")) {
+  transport = new MotorcycleTransport();
+} else {
+  console.log("Selecione o tipo de entrega.");
+}
+
+transport && transport.startTransport();
